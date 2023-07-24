308e356eb80f68ad678c81ef0d76aa5f2a7a9066
kesavan <kesavanplk7@gmail.com>
2023-07-24 14:18:21 +0530

    install react-icons

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,18 @@
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="navbar">
+    <div className="logo">
+      <BiLogoDigg
+    </div>
+    <div className="icons">
+      <div className="notification">
+        <span>1</span>
+      </div>
+      <div className="user">
+        <span></span>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Navbar;
