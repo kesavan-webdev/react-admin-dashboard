37b15d3214c3a549a4fc3ddf5d04e8f0de81f0f7
kesavan <kesavanplk7@gmail.com>
2023-07-24 14:46:45 +0530

    add navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,18 +1,33 @@
+import "../navbar/navbar.scss";
+import { BiLogoDigg, BiSearch } from "react-icons/bi";
+import { MdWidgets, MdOutlineSettings } from "react-icons/md";
+import { IoMdQrScanner, IoMdNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
-  return <div className="navbar">
-    <div className="logo">
-      <BiLogoDigg
-    </div>
-    <div className="icons">
-      <div className="notification">
-        <span>1</span>
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <BiLogoDigg />
+        <span>logo</span>
       </div>
-      <div className="user">
-        <span></span>
+      <div className="icons">
+        <BiSearch />
+        <MdWidgets />
+        <IoMdQrScanner />
+
+        <div className="notification">
+          <IoMdNotificationsOutline />
+          <span className="count">1</span>
+        </div>
+        <div className="user">
+          <CgProfile className="img" />
+          <span>kesav</span>
+        </div>
+        <MdOutlineSettings />
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Navbar;
